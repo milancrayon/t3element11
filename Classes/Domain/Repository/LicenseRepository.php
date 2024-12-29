2d95687e247f0de0c3c082f309ae97f8e94106a6
Milan <milan.crayon@gmail.com>
2024-12-29 18:26:39 +0530

    Updates

--- Classes/Domain/Repository/LicenseRepository.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Crayon\T3element\Domain\Repository;
-
-class LicenseRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
-{
-}
--- Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined('TYPO3_MODE') || die();
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('t3element', 'Configuration/TypoScript', 'T3element');
